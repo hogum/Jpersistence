44cbf93c47d81d61c1bb989c077bda91ca83951d
ks <mugoh.ks@gmail.com>
2018-12-18 19:28:18 +0300

    Add display pane and panel

--- cardBuilder/CardPlayer.java
+++ cardBuilder/CardPlayer.java
@@ -1,3 +1,63 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JFileChooser;
+
+import java.util.ArrayList;
+
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
 public class CardPlayer {
     
+    private JFrame frame;
+    private JPanel panel;
+    private JTextArea displayArea;
+    private JTextArea answerArea;
+    private JButton cardButton;
+    private boolean isAnswer;
+    private int cardIndex;
+    private DrawCard currentCard;
+    private ArrayList<DrawCard> cardList;
+
+    public void createDisplay() {
+
+        frame = new JFrame("Card PLayer");
+        JPanel panel = new JPanel();
+
+        Font cardFont = new Font("cambria", Font.PLAIN, 18);
+
+        displayArea = new JTextArea(10, 20);
+        displayArea.setFont(cardFont);
+        displayArea.setLineWrap(true);
+        displayArea.setEditable(false);
+
+        JScrollPane displayScrollPane = new JScrollPane(
+            displayArea);
+        displayScrollPane.setVerticalScrollBarPolicy(
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        displayScrollPane.setHorizontallScrollBarPolicy(
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        cardButton = new JButton("See Question");
+        cardButton.addActionListener(new CardButtonListener());
+        
+        panel.add(displayScrollPane);
+        panel.add(cardButton);
+
+    }  
 }
